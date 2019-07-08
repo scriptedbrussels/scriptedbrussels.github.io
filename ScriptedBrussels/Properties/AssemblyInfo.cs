4366a840bc51cd3d2504c58a89301e8d21d3439f
mihai stancu <stancu.mihai.dorian@gmail.com>
2019-07-08 17:49:03 +0200

    Create empty web api SPA

--- ScriptedBrussels/Properties/AssemblyInfo.cs
+++ ScriptedBrussels/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("5740574d-4976-4fd5-a46d-8e5c4f5af057")]
+[assembly: Guid("c7104b00-92b5-4aa5-b04b-964343f3b1a9")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,8 +29,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
